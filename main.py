731decc5c60d74da3d9a4889143f298b2dfe65ff
Vignie Babul <vigniebabul.dev@gmail.com>
2026-03-31 12:17:09 +0200

    Add tests for localization and drone stats configs

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from src.config import PATHS
+from pprint import pprint
+
+from src.config import PATHS, JSONConfig, Localization
 from src.data import Analitycs, DataSave
 from src.utils import get_iso_datetime
 
+
+def local_config_test() -> None:
+	local = Localization(PATHS['conf']['local'])
+	local.dump({
+		'ru': {'foo': 'бар', 'hello_world': 'Привет, Мир!'},
+		'en': {'foo': 'bar', 'hello_world': 'Hello! World!'},
+	})
+	pprint(local.load())
+	pprint(local.load('ru'))
+	pprint(local.load('en'))
+	print(local.load('en')['hello_world'])
+	print()
+
+def drone_config_test() -> None:
+	drone = JSONConfig(PATHS['conf']['drone'])
+	drone.dump({
+		'max_speed': 0,
+		'acceleration': 0,
+		'rotation_speed': 0,
+		'battery_life': 0,
+		'obstacle_penalty': 0,
+	})
+	pprint(drone.load())
+	print()
 
 def analitycs_test() -> None:
 	analitycs = Analitycs(
@@ -31,5 +57,7 @@ def data_save_test() -> None:
 
 
 if __name__ == '__main__':
+	local_config_test()
+	drone_config_test()
 	analitycs_test()
 	data_save_test()
